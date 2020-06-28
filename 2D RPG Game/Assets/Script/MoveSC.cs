bb8ebd5adef94bb623779397c3180f2c345f2eb7
bodelim <bodle@github.com>
2020-06-28 23:16:52 +0900

    Add Project File
    Yap!

---
+++ 2D RPG Game/Assets/Script/MoveSC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSC : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    //void Update()
+    //{
+    //    if(Input.GetButtonUp("Horizontal")) {
+    //        rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+    //    }
+    //}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        
+        rigid.AddForce(Vector2.right *h, ForceMode2D.Impulse);
+
+        if(rigid.velocity.x > maxSpeed)//Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if(rigid.velocity.x < maxSpeed*(-1))//Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+        
+    }
+
+
+}
---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 vec = new Vector3(5, 0, 0);
+        transform.Translate(vec);
+        
+    }
+}
