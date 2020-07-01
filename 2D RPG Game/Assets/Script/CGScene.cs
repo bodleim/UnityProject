0e37f7410896b284427b7e6b15438159fe47c1e5
bodelim <bodle@github.com>
2020-07-02 02:29:50 +0900

    메인화면 추가

---
+++ 2D RPG Game/Assets/Script/CGScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CGScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        public void Click(int scnumber)
+    {
+        SceneManager.LoadScene(scnumber);
+    }
+
+}
